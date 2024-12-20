7d4771d30c11bd707f3b839d0b4696856b109c6f
mattia-tatone <mattiatatone@hotmail.it>
2024-12-20 18:57:06 +0100

    export accessi

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { DatabaseUpdater } from "./DatabaseUpdater";
 import { ExecutionTimeLogger } from "./ExecutionTimeLogger"
 import { LogLevels, Logger } from "./Logger";
+import {AccessiModule} from "./accessi-module/AccessiModule"
 import { Orm } from "./Orm";
 import { DateUtilities, RestUtilities, DatabaseUtilities, StatusCode } from "./Utilities";
 export * from "es-node-firebird";
 import autobind from "autobind-decorator";
-export { autobind, ExecutionTimeLogger, Logger, LogLevels, Orm, DateUtilities, RestUtilities, DatabaseUtilities, DatabaseUpdater, StatusCode };
+export { autobind, ExecutionTimeLogger, Logger, LogLevels, Orm, DateUtilities, RestUtilities, DatabaseUtilities, DatabaseUpdater, StatusCode, AccessiModule };
