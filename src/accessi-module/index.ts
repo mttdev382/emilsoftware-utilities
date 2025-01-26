f46ba52b05a30d4603c723238d5296ed9cfe61ff
mattia-tatone <mattiatatone@hotmail.it>
2025-01-26 19:49:58 +0100

    export statoregistrazione

--- src/accessi-module/index.ts
+++ src/accessi-module/index.ts
@@ -1,3 +1,4 @@
+export { StatoRegistrazione } from "./models/StatoRegistrazione";
 export { AccessiModule } from "./AccessiModule";
 export {LoginRequest} from "./models/DTO/LoginRequest"
 export {LoginResponse} from "./models/DTO/LoginResponse"
