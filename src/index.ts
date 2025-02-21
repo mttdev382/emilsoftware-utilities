f1531951f70fd005c87a2c377c579301b675340d
mattia-tatone <mattiatatone@hotmail.it>
2025-02-21 18:53:11 +0100

    fix index

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@ import { ExecutionTimeLogger } from "./ExecutionTimeLogger"
 import { LogLevels, Logger } from "./Logger";
 import { Orm } from "./Orm";
 import { DateUtilities, RestUtilities, DatabaseUtilities, StatusCode } from "./Utilities";
-import { AccessiModule, StatoRegistrazione } from "./accessi-module";
-import { AccessiModel } from "./accessi-module/AccessiModel";
 import { autobind } from "./autobind";
 
 
-export { AccessiModule, StatoRegistrazione, LoginRequest, LoginResponse } from "./accessi-module";
+export { AccessiModule, StatoRegistrazione } from "./accessi-module";
 
 export * from "es-node-firebird";
 export { autobind, ExecutionTimeLogger, Logger, LogLevels, Orm, DateUtilities, RestUtilities, DatabaseUtilities, DatabaseUpdater, StatusCode };
