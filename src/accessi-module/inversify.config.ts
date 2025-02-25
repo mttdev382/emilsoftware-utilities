b4e4d11c19172309631dcd8f6258235ba3da71b0
mattia-tatone <mattiatatone@hotmail.it>
2025-02-25 23:05:36 +0100

    fix

--- src/accessi-module/inversify.config.ts
+++ src/accessi-module/inversify.config.ts
@@ -12,35 +12,74 @@ import { AccessiOptions, AccessiModule } from "./AccessiModule";
 import { AccessiRoutes } from "./AccessiRoutes";
 import { AccessiControllerBase } from "./AccessiControllerBase";
 
-
 const container = new Container({ defaultScope: "Singleton" });
 
+console.log("🔍 Verifica delle importazioni:");
+console.log("UserService:", UserService);
+console.log("AuthService:", AuthService);
+console.log("PermissionService:", PermissionService);
+console.log("EmailService:", EmailService);
+console.log("AccessiController:", AccessiController);
+console.log("AccessiModule:", AccessiModule);
+console.log("AccessiRoutes:", AccessiRoutes);
+console.log("AccessiControllerBase:", AccessiControllerBase);
+
+if (
+  !UserService ||
+  !AuthService ||
+  !PermissionService ||
+  !EmailService ||
+  !AccessiController ||
+  !AccessiModule ||
+  !AccessiRoutes ||
+  !AccessiControllerBase
+) {
+  throw new Error("ERRORE: Una o più dipendenze non sono state importate correttamente!");
+}
+
+// Definizione delle opzioni per AccessiModule
 const accessiOptions: AccessiOptions = {
-    databaseOptions: {},
-    mockDemoUser: false,
-    encryptionKey: "",
-    jwtOptions: { secret: "", expiresIn: "1h" },
-    emailOptions: {
-        auth: {
-            user: "",
-            pass: ""
-        },
-        host: "",
-        port: 0,
-        secure: false
-    }
+  databaseOptions: {},
+  mockDemoUser: false,
+  encryptionKey: "",
+  jwtOptions: { secret: "", expiresIn: "1h" },
+  emailOptions: {
+    auth: {
+      user: "",
+      pass: ""
+    },
+    host: "",
+    port: 0,
+    secure: false
+  }
 };
 
-console.log("AccessiOptions:", accessiOptions);
+console.log("AccessiOptions inizializzato correttamente:", accessiOptions);
 container.bind<AccessiOptions>("AccessiOptions").toConstantValue(accessiOptions);
 
+console.log("Registrazione delle dipendenze...");
 
+console.log("Binding IUserService...");
 container.bind<IUserService>("IUserService").to(UserService);
+
+console.log("Binding IAuthService...");
 container.bind<IAuthService>("IAuthService").to(AuthService);
+
+console.log("Binding IPermissionService...");
 container.bind<IPermissionService>("IPermissionService").to(PermissionService);
+
+console.log("Binding IEmailService...");
 container.bind<IEmailService>("IEmailService").to(EmailService);
-container.bind<AccessiControllerBase>("AccessiControllerBase").to(AccessiController);
+
+console.log("Binding AccessiControllerBase...");
+container.bind<AccessiControllerBase>(AccessiControllerBase).to(AccessiController);
+
+console.log("Binding AccessiRoutes...");
 container.bind<AccessiRoutes>(AccessiRoutes).toSelf();
+
+console.log("Binding AccessiModule...");
 container.bind<AccessiModule>(AccessiModule).toSelf();
+
+console.log("Tutte le dipendenze sono state registrate correttamente!");
 
 export { container };
