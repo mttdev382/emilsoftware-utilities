6874bb87029068c2e06477ea6d9db1d998f1f889
mttdev <mattiatatone@hotmail.it>
2025-02-28 10:53:31 +0100

    init

--- src/accessi-module/index.ts
+++ src/accessi-module/index.ts
@@ -3,15 +3,27 @@ import { AccessiModule, AccessiOptions } from "./AccessiModule";
 import { NestFactory } from "@nestjs/core";
 import { ExpressAdapter } from "@nestjs/platform-express";
 import { serveSwaggerDocs } from "./swagger/SwaggerConfig";
-
+import * as express from "express";
 export { StatoRegistrazione } from "./models/StatoRegistrazione";
 export { AccessiModule } from "./AccessiModule";
 export { ILoginResult } from "./Services/AuthService/IAuthService"
 
 
 
-export async function initializeAccessiModule(app: Application, options: AccessiOptions) {
-    const nestApp = await NestFactory.create(AccessiModule.forRoot(options), new ExpressAdapter(app));
+export async function initializeAccessiModule(app: Application, options: any) {
+    // 1. Creiamo un'istanza di ExpressAdapter basata su `app`
+    const expressAdapter = new ExpressAdapter(app);
+  
+    // 2. Creiamo Nest con l'ExpressAdapter
+    const nestApp = await NestFactory.create(AccessiModule.forRoot(options), expressAdapter);
+  
+    // 3. Abilitiamo i middleware di Express (es. body-parser)
+    nestApp.use(express.json());
+    nestApp.use(express.urlencoded({ extended: true }));
+  
+    // 4. Avviamo NestJS (senza avviare un server separato)
     await nestApp.init();
+  
+    // 5. Serviamo automaticamente Swagger
     serveSwaggerDocs(nestApp);
-}
+  }
