170115205ebb15337180f1a10920df0c22ed5b86
mttdev <mattiatatone@hotmail.it>
2025-03-03 08:41:55 +0100

    initialize module

--- src/accessi-module/index.ts
+++ src/accessi-module/index.ts
@@ -1,29 +1,25 @@
 import { Application } from "express";
-import { AccessiModule, AccessiOptions } from "./AccessiModule";
+import { AccessiModule } from "./AccessiModule";
 import { NestFactory } from "@nestjs/core";
 import { ExpressAdapter } from "@nestjs/platform-express";
 import { serveSwaggerDocs } from "./swagger/SwaggerConfig";
-import * as express from "express";
 export { StatoRegistrazione } from "./models/StatoRegistrazione";
 export { AccessiModule } from "./AccessiModule";
 export { ILoginResult } from "./Services/AuthService/IAuthService"
 
-
-
-export async function initializeAccessiModule(app: Application, options: any) {
-    // 1. Creiamo un'istanza di ExpressAdapter basata su `app`
+  export async function initializeAccessiModule(app: Application, options: any) {
     const expressAdapter = new ExpressAdapter(app);
-  
-    // 2. Creiamo Nest con l'ExpressAdapter
-    const nestApp = await NestFactory.create(AccessiModule.forRoot(options), expressAdapter);
-  
-    // 3. Abilitiamo i middleware di Express (es. body-parser)
-    nestApp.use(express.json());
-    nestApp.use(express.urlencoded({ extended: true }));
-  
-    // 4. Avviamo NestJS (senza avviare un server separato)
+
+    const nestApp = await NestFactory.create(AccessiModule.forRoot(options), expressAdapter, { bufferLogs: true });
+
+    // Monta NestJS solo su /api/accessi
+    app.use('/api/accessi', expressAdapter.getInstance());
+
+    nestApp.enableCors();
+    nestApp.setGlobalPrefix('/accessi'); // Rimuovilo se causa problemi
+
     await nestApp.init();
-  
-    // 5. Serviamo automaticamente Swagger
+
+    // Servi Swagger solo per Accessi
     serveSwaggerDocs(nestApp);
-  }
+}
